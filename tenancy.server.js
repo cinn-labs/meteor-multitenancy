0c7373fe78cd1d1a29622de54ea45def4d5b488c
bratelefant <59534030+bratelefant@users.noreply.github.com>
2022-04-21 15:37:21 +0200

    upsert removed, problems with unique index on inserts

--- tenancy.server.js
+++ tenancy.server.js
@@ -1,40 +1,52 @@
-import { Meteor } from 'meteor/meteor';
-import Tenancy from './tenancy.common';
+import { Meteor } from "meteor/meteor";
+import Tenancy from "./tenancy.common";
 
-Meteor.publish('_groupId', function () {
+Meteor.publish("_groupId", function () {
   if (!this.userId) return this.ready();
-  return Meteor.users.find({ _id: this.userId }, { fields: { [Tenancy.groupName]: 1 } });
+  return Meteor.users.find(
+    { _id: this.userId },
+    { fields: { [Tenancy.groupName]: 1 } }
+  );
 });
 
-Tenancy.setUserTenant = function(userId, groupId, callback) {
-  Meteor.defer(() => Meteor.users.update({ _id: userId }, { $set: { [Tenancy.groupName]: groupId } }, callback));
+Tenancy.setUserTenant = function (userId, groupId, callback) {
+  Meteor.defer(() =>
+    Meteor.users.update(
+      { _id: userId },
+      { $set: { [Tenancy.groupName]: groupId } },
+      callback
+    )
+  );
 };
 
-Tenancy.insertHook = function(userId, doc) {
+Tenancy.insertHook = function (userId, doc) {
   const groupId = Tenancy.currentGroupId(userId);
-  if(!groupId) return true;
+  if (!groupId) return true;
   doc[Tenancy.groupName] = groupId;
 };
 
-Tenancy.upsertHook = function(userId, selector, modifier, options) {
+Tenancy.upsertHook = function (userId, selector, modifier, options) {
   const groupId = Tenancy.currentGroupId(userId);
-  if(!groupId) return true;
+  if (!groupId) return true;
   modifier.$set = modifier.$set || {};
   modifier.$set[Tenancy.groupName] = groupId;
 };
 
-Tenancy.findHook = function(userId, selector, options) {
+Tenancy.findHook = function (userId, selector, options) {
   const groupId = Tenancy.currentGroupId(userId);
-  if(!groupId) return true;
+  if (!groupId) return true;
   selector[Tenancy.groupName] = groupId;
 };
 
-Tenancy.prepareCollection = function(collection) {
-  if(!collection.before) return console.warn(`[TENANCY] Collection ${collection._name} was not succesfully configured.`);
+Tenancy.prepareCollection = function (collection) {
+  if (!collection.before)
+    return console.warn(
+      `[TENANCY] Collection ${collection._name} was not succesfully configured.`
+    );
   collection.before.find(Tenancy.findHook);
   collection.before.findOne(Tenancy.findHook);
   collection.before.insert(Tenancy.insertHook);
-  collection.before.upsert(Tenancy.upsertHook);
+  //collection.before.upsert(Tenancy.upsertHook);
 };
 
-if(!!Meteor.users) Meteor.users.before.find(Tenancy.findHook);
+if (!!Meteor.users) Meteor.users.before.find(Tenancy.findHook);
